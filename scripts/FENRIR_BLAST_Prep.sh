2c03f352f2721ae34f5ac072523475018b9b9e79
Farrah Roy <froy@hsph.harvard.edu>
2020-04-13 14:21:41 -0400

    Update FENRIR_BLAST_Prep.sh

--- scripts/FENRIR_BLAST_Prep.sh
+++ scripts/FENRIR_BLAST_Prep.sh
@@ -1,3 +1,24 @@
+#Description: Extracts reads aligning to exogenous or endogenous virus
+
+#Input:
+  #1) Reads in list of endogenous and exogenous viruses
+  #2) Reference genome and pseudovirome
+  #3) list of proviruses of interest, otherwise use the one provided
+  #4) Depending on size of the reference genome, index files for HISAT2
+  #5) .bed file containing tandem repeats in pseudovirome
+  #6) .bed of ERVs, otherwise use the one provided
+  #7) Variables.txt - filled out to match your experimental design
+
+#Output:
+  #1) combined counts matrix for endogenous and exogenous viruses detected in RNA-seq, respectively
+  #2) graphed viral counts for endogenous and exogenous viruses
+  #3) percent coverage matricies for endogenous and exogenous viruses
+  #4) graphed coverage of endogenous and exogenous viruses
+  #5) reads aligning to a given virus stored in a .fasta for BLAST verification
+
+#Author:
+  #Farrah Roy
+
 #!/bin/bash
 source Variables.txt
 echo $READING
